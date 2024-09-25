ff0bff14dee7428ae8d07a0a82385286d4de0b03
pplg112 <114993046+pplg112@users.noreply.github.com>
2024-09-25 22:31:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+} else {
+    console.error("ไม่พบ element hamburger หรือ nav-menu");
+}
